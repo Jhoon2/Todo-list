394fa44b2b4866c4ab36f788eb1d2b97e7e164b8
Jhoon2 <wjh2144@naver.com>
2022-10-17 18:39:55 +0900

    [fix]용도 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import './App.css';
-import Home from './components/pages/TodoList';
 import { Routes, Route } from 'react-router-dom';
-import Detail from './components/detail/Detail';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />}></Route>
-    <Route path="/detail/:id" element={<Detail />}></Route>
-  </Routes>
-);
+const App = () => <div>ㅎㅇ</div>;
 
 export default App;
